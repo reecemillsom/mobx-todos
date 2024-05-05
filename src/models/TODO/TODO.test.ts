4d944b9ed4ad98c3da9cb232bd25cfecc5692489
reecemillsom <reecemillsom@gmail.com>
2024-05-05 17:25:10 +0100

    master
    1. Adding tests for the TODO class, each individual item.

--- src/models/TODO/TODO.test.ts
+++ src/models/TODO/TODO.test.ts
@@ -8,7 +8,12 @@ describe('TODO', () => {
 
                 expect(todo).toEqual({
                     id: expect.any(String),
-                    text: '',
+                    text: {
+                        original: '',
+                        updated: ''
+                    },
+                    deleted: false,
+                    creating: false,
                     editing: false,
                     completed: false,
                 });
@@ -16,34 +21,118 @@ describe('TODO', () => {
         });
 
         describe('when its a already existing TODO item', () => {
-            const context = {
-                text: 'First TODO item',
-                completed: true,
-            };
+            it('will set the initial state correctly', () => {
+                const context = {
+                    text: 'First TODO item',
+                    completed: true,
+                };
 
-            const todo = new TODO(context);
+                const todo = new TODO(context);
 
-            expect(todo).toEqual({
-                id: expect.any(String),
-                text: 'First TODO item',
-                editing: false,
-                completed: true,
+                expect(todo).toEqual({
+                    id: expect.any(String),
+                    text: {
+                        original: 'First TODO item',
+                        updated: ''
+                    },
+                    deleted: false,
+                    creating: false,
+                    editing: false,
+                    completed: true
+                });
             });
         });
-
     });
 
     describe('when actions are called', () => {
         it('should update what the value in state', () => {
             const todo = new TODO();
 
-            // todo.setText('Text value');
+            todo.setText('Text value', 'original');
             todo.setEditing(true);
             todo.setCompleted();
+            todo.setCreating(true);
+            todo.setDeleted();
 
-            expect(todo.getText()).toEqual('Text value');
+            expect(todo.getText()).toEqual({
+                original: 'Text value',
+                updated: ''
+            });
             expect(todo.getEditing()).toEqual(true);
             expect(todo.getCompleted()).toEqual(true);
+            expect(todo.getCreating()).toEqual(true);
+            expect(todo.getDeleted()).toEqual(true);
+        });
+    });
+
+    describe('when accepting an edit', () => {
+        it('should set the updated and original text value accordingly, and editing state', () => {
+            const todo = new TODO();
+
+            todo.setText('Value text', 'updated');
+            todo.setCreating(true);
+
+            todo.acceptEdit();
+
+            expect(todo.getText()).toEqual({
+                original: 'Value text',
+                updated: '',
+            });
+            expect(todo.getEditing()).toEqual(false);
+        });
+    });
+
+    describe('when cancelling an edit', () => {
+        it('should set the text and editing state', () => {
+            const todo = new TODO();
+
+            todo.setText('Value text', 'original');
+            todo.setEditing(true);
+
+            todo.cancelEdit();
+
+            expect(todo.getText()).toEqual({
+                original: 'Value text',
+                updated: 'Value text'
+            });
+
+            expect(todo.getEditing()).toEqual(false);
+        });
+    });
+
+    describe('when accepting a creation', () => {
+        it('should set the text and creation state', () => {
+            const todo = new TODO();
+
+            todo.setText('Value text', 'updated');
+            todo.setCreating(true);
+
+            todo.acceptCreate();
+
+            expect(todo.getText()).toEqual({
+                original: 'Value text',
+                updated: ''
+            });
+
+            expect(todo.getCreating()).toEqual(false);
+        });
+    });
+
+    describe('when cancelling a creation', () => {
+        it('should set the text and creation state', () => {
+            const todo = new TODO();
+
+            todo.setText('Value text', 'updated');
+            todo.setCreating(true);
+
+            todo.cancelCreate();
+
+            expect(todo.getText()).toEqual({
+                original: '',
+                updated: ''
+            });
+
+            expect(todo.getCreating()).toEqual(false);
         });
     });
 });
